89f45983a1a420e8131813fbb90466a5dd379bc9
ma4362531-byte <ma4362531@gmail.com>
2026-08-08 03:53:34 +0300

    Question 2 Part1

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -17,6 +17,25 @@ namespace OOP02
             //Classes are more suitable for large applications because they support important OOP concepts such as inheritance, polymorphism, and encapsulation.They also make code easier to reuse, organize, maintain, and extend.
             #endregion
 
+            #region Question 2 Part1
+            //a) Which class is the parent class?
+
+            //Shipment is the parent class.
+
+            //b) Which class is the child class?
+
+            //ExpressShipment is the child class.
+
+            //c) What members are inherited by ExpressShipment?
+
+            //ExpressShipment inherits the TrackingCode property from the Shipment class.
+
+            //d) Why is inheritance better than duplicating the same code in multiple classes?
+
+            //Inheritance reduces code duplication, allows code reuse, and makes the code easier to maintain and extend.
+
+            #endregion
+
 
 
 
