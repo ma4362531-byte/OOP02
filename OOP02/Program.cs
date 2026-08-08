3f88ba564f8ba5b50891790380609e04c21ab33b
ma4362531-byte <ma4362531@gmail.com>
2026-08-08 03:42:17 +0300

    Question 1 Part 1

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace OOP02
+﻿using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace OOP02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Question 1 Part 1
+            //a) What is the difference between a class and a struct?
+
+            //A class is a reference type that is used to create objects and is suitable for complex data and large applications.A struct is a value type that is mainly used for small and simple data structures.
+
+            //b) Why are classes more suitable than structs for large applications?
+
+            //Classes are more suitable for large applications because they support important OOP concepts such as inheritance, polymorphism, and encapsulation.They also make code easier to reuse, organize, maintain, and extend.
+            #endregion
+
+
+
+
+
         }
     }
 }
